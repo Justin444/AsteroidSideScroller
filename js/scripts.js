72f8d5cfd7667d50e28508a9d1af708cf0281267
Justin444 <36039121+Justin444@users.noreply.github.com>
2018-02-13 10:06:14 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,58 @@
 //variables
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var player = new createObject("ship.png", 0, 0);
+var player = new createObject("ship.png", 0, 0, 50, 50);
+var laser = new createObject("laser.png",player.Xpos, player.Ypos, 50, 50);
+var asteroid = [4];
+//var laser = [];
+
+for(var x=0; x < 4; x++)
+{
+	var rnd = Math.floor(100 + Math.random() * 500);
+	asteroid[x] = new createObject("asteroid.png", rnd, rnd, 75, 75);
+}
+
+//fps
+var fps = 30;
+var interval = 1000/fps; // milliseconds
+var step = interval/1000 // seconds
 
 //controls
 var left = false;
 var right = false;
 var up = false;
 var down = false;
+var fire = false;
+var stopped = false;
 
 //calls main
 main();
 
-/*
-var fps = 30;
-var interval = 1000/fps; // milliseconds
-var step = interval/1000 // seconds
-*/
-
 function main()
 {
+	//clear the canvas
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+	
+	//side scrolling
+	for (var x = 0; x < 4; x++)
+	{
+		asteroid[x].Xpos += -player.Xpos / 50;
+		asteroid[x].Ypos += -player.Ypos/ 50;
+	}
+	
+	//collision check
+	for(var i = 0; i < 4; i++)
+	{
+		if(player.Xpos < asteroid[i].Xpos + asteroid[i].width &&
+		player.Xpos + player.width > asteroid[i].Xpos&&
+		player.Ypos < asteroid[i].Ypos + asteroid[i].height &&
+		player.height+ player.Ypos > asteroid[i].Ypos)
+		{
+			player.Xpos = 0;
+			player.Ypos = 0;
+		}
+	}
+	
 	//speed should be changed
 	if(left)
 		player.Xpos -= 10;
@@ -29,24 +62,59 @@ function main()
 		player.Xpos += 10;
 	if(down)
 		player.Ypos += 10;
+	if(fire)
+	{
+		for(var x=0; x < canvas.width; x++)
+		{
+			laser.Xpos += 10;
+			if (laser.Xpos == 500)
+			{
+				fire = false;
+				laser.Xpos = player.Xpos;
+				laser.Ypos = player.Ypos;
+			}
+		}
+		ctx.drawImage(laser.Sprite, laser.Xpos, laser.Ypos, 50, 50);
+	}
+
 	
-	//clear the canvas
-	ctx.clearRect(0,0,canvas.width, canvas.height);
 	//			  player object starting x and y coord size of ship 
 	ctx.drawImage(player.Sprite,player.Xpos,player.Ypos, 50, 50);
+	//asteroids
+	for (var x=0; x < 4; x++)
+	ctx.drawImage(asteroid[x].Sprite, asteroid[x].Xpos, asteroid[x].Ypos, 75, 75);
 }
 //time to update frames
 //needs to be changed
 setInterval(main, 10);
 
 //use this for creating objects
-function createObject(img, x, y)
+function createObject(img, x, y, w, h)
 {
 	this.Sprite = new Image();
 	this.Sprite.src = img;
 	this.Xpos = x;
 	this.Ypos = y;
+	this.width = w;
+	this.height = h;
 }
+//laser
+/*function Laser(){
+	var laser = new createObject("laser.png", player.Xpos, player.Ypos, 10, 25);
+	for (var x = 0; x < canvas.width; x++)
+		laser.Xpos += x;
+	//this.setBoundAction(DESTROY);
+	//this.setAngle(Sprite.getImgAngle());
+	//this.setSpeed(10);
+}// end fire*/
+
+//controls
+	window.addEventListener("keypress", function(e){
+		if(e.keyCode == 32)
+		{
+			fire = true;
+		}
+	}, false);
 
 	//controls
 	window.addEventListener("keydown", function(e){
@@ -81,6 +149,6 @@ function createObject(img, x, y)
 				break;
 			case 40: // down arrow
 				down = false;
-				break;	
+				break
 		}
 	}, false);
