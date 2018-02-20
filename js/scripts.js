c7329d5021f885aabaed27776d51cdd0e3ee4a8f
Justin444 <36039121+Justin444@users.noreply.github.com>
2018-02-20 11:23:26 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,14 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var player = new createObject("ship.png", 0, 0, 50, 50);
-var laser = new createObject("laser.png",player.Xpos, player.Ypos, 50, 50);
-var asteroid = [4];
-//var laser = [];
-
-for(var x=0; x < 4; x++)
-{
-	var rnd = Math.floor(100 + Math.random() * 500);
-	asteroid[x] = new createObject("asteroid.png", rnd, rnd, 75, 75);
-}
+var asteroid = [];
+var lasers = [];
+var asterPos = 0;
+var laspos = 0;
+var start = false;
 
 //fps
-var fps = 30;
+var fps = 60;
 var interval = 1000/fps; // milliseconds
 var step = interval/1000 // seconds
 
@@ -23,25 +19,41 @@ var right = false;
 var up = false;
 var down = false;
 var fire = false;
-var stopped = false;
 
 //calls main
 main();
 
 function main()
 {
+	if  (start == false)
+	{
+	 startScreen();
+	}
+	else
+	{
 	//clear the canvas
 	ctx.clearRect(0,0,canvas.width, canvas.height);
 	
-	//side scrolling
-	for (var x = 0; x < 4; x++)
+	//generating asteroids
+	var amt = Math.floor(5 + Math.random() * 15);
+	
+	if(asterPos == 0)
 	{
+		for(var x = 0; x < amt; x++)
+		{
+			createAsteroid()
+			asterPos++;
+		}
+	}
+	//side scrolling
+	for (var x = 0; x < asterPos; x++)
+	{
+		ctx.drawImage(asteroid[x].Sprite, asteroid[x].Xpos, asteroid[x].Ypos, 75, 75);
 		asteroid[x].Xpos += -player.Xpos / 50;
-		asteroid[x].Ypos += -player.Ypos/ 50;
 	}
 	
 	//collision check
-	for(var i = 0; i < 4; i++)
+	for(var i = 0; i < asterPos; i++)
 	{
 		if(player.Xpos < asteroid[i].Xpos + asteroid[i].width &&
 		player.Xpos + player.width > asteroid[i].Xpos&&
@@ -55,7 +67,7 @@ function main()
 	
 	//speed should be changed
 	if(left)
-		player.Xpos -= 10;
+		player.Xpos -= step * 300;
 	if(up)
 		player.Ypos -= 10;
 	if(right)
@@ -64,29 +76,25 @@ function main()
 		player.Ypos += 10;
 	if(fire)
 	{
-		for(var x=0; x < canvas.width; x++)
-		{
-			laser.Xpos += 10;
-			if (laser.Xpos == 500)
-			{
-				fire = false;
-				laser.Xpos = player.Xpos;
-				laser.Ypos = player.Ypos;
-			}
-		}
-		ctx.drawImage(laser.Sprite, laser.Xpos, laser.Ypos, 50, 50);
+		createLaser();
+		laspos++;
+		fire = false;
 	}
+	for (var x = 0; x < laspos; x++)
+		{
+			ctx.drawImage(lasers[x].Sprite, lasers[x].Xpos, lasers[x].Ypos, 50, 50);
+			lasers[x].Xpos += step * 1500;
+		}
 
 	
 	//			  player object starting x and y coord size of ship 
 	ctx.drawImage(player.Sprite,player.Xpos,player.Ypos, 50, 50);
-	//asteroids
-	for (var x=0; x < 4; x++)
-	ctx.drawImage(asteroid[x].Sprite, asteroid[x].Xpos, asteroid[x].Ypos, 75, 75);
+	}
+
 }
 //time to update frames
 //needs to be changed
-setInterval(main, 10);
+setInterval(main, interval);
 
 //use this for creating objects
 function createObject(img, x, y, w, h)
@@ -98,28 +106,29 @@ function createObject(img, x, y, w, h)
 	this.width = w;
 	this.height = h;
 }
-//laser
-/*function Laser(){
-	var laser = new createObject("laser.png", player.Xpos, player.Ypos, 10, 25);
-	for (var x = 0; x < canvas.width; x++)
-		laser.Xpos += x;
-	//this.setBoundAction(DESTROY);
-	//this.setAngle(Sprite.getImgAngle());
-	//this.setSpeed(10);
-}// end fire*/
 
-//controls
-	window.addEventListener("keypress", function(e){
-		if(e.keyCode == 32)
-		{
-			fire = true;
-		}
-	}, false);
+function createAsteroid()
+{
+	var rndX = Math.floor(100 + Math.random() * 500);
+	var rndY = Math.floor(100 + Math.random() * 500);
+	asteroid[asterPos] = new createObject("asteroid.png", rndX, rndY, 75, 75);
+}
+
+function createLaser()
+{
+	lasers[laspos] = new createObject("laser.png", player.Xpos, player.Ypos, 10, 25);
+}// end fire
 
 	//controls
 	window.addEventListener("keydown", function(e){
 		switch(e.keyCode)
 		{
+			case 13:
+				start = true;
+				break;
+			case 32:
+				fire  = true;
+				break;
 			case 37: // left arrow
 				left = true;
 				break;
@@ -138,6 +147,9 @@ function createObject(img, x, y, w, h)
 	window.addEventListener("keyup", function(e){
 		switch(e.keyCode)
 		{
+			case 32: //space
+				fire  = false;
+				break;
 			case 37: // left arrow
 				left = false;
 				break;
@@ -149,6 +161,39 @@ function createObject(img, x, y, w, h)
 				break;
 			case 40: // down arrow
 				down = false;
-				break
+				break;
 		}
 	}, false);
+
+	
+function startScreen()
+{
+	 // the red component of rgb
+	 var hue = 255; 
+	 // create a css color from the `hue`
+	 var color = 'rgb(' + hue + ',0,0)';
+	/*make instructions flash
+    // are we moving toward red or black?
+    var direction = 1; 
+		
+	hue += direction;
+        if (hue > 255) direction = -1;
+        if (hue < 0) direction = 1;
+	*/
+	//find the center of the canvas
+	var x = canvas.width / 2;
+	var y = canvas.height / 2;
+	
+	//clear the canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	//Write the game title
+	ctx.font = "50px Garamond";
+	ctx.fillStyle = 'red';
+	ctx.fillText("Asteroids Side Scroller",100,y);
+	
+	//Write start instructions
+	ctx.font = "25px Garamond";
+	ctx.fillStyle = color;
+	ctx.fillText("Press Enter to begin",170,y+50);
+}
